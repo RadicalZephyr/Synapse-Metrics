97e5c9ec6af08664c8408e4a53342ef594590599
Geoff Shannon <earthlingzephyr@gmail.com>
2012-06-22 13:02:46 -0700

    Made dummy interface use RowTable; Changed whitespace to all spaces

--- src/main/java/org/sagebionetworks/metrics/client/Metrics.java
+++ src/main/java/org/sagebionetworks/metrics/client/Metrics.java
@@ -1,65 +1,64 @@
 package org.sagebionetworks.metrics.client;
 
+import org.sagebionetworks.metrics.client.widget.RowTable;
+import static org.sagebionetworks.metrics.client.widget.RowTable.Row;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Metrics implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-	  String[] testUsersUCSC = new String[] {"Users", "jlong", "kellrott", "elinor", "ted"};
-	  String[] testUserDateUCSC = new String[] {"Days", "14-May-2012", "10-May-2012", "10-May-2012", "01-May-2012"};
-	  
-	  String[] testUsersSage = new String[] {"Users", "chris.gaiteri", "xudong.dai", "charles.ferte", "ben.sauerwine", "in.sock.jang", "erich.huang", "lara.mangravite"};
-	  String[] testUserDateSage = new String[] {"Days", "18-May-2012", "16-Apr-2012", "27-Mar-2012", "23-Apr-2012", "15-Mar-2012", "14-Mar-2012", "15-Mar-2012", "25-Mar-2012"};
-	  
-	  String[] projects = new String[] {"Name", "Metagenomics", "Synapse Umbrella"};
-	  String[] projectScore = new String[] {"Score", "11", "4"};
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+        Row[] ucscData = new Row[] {new Row("jlong", "14-May-2012"),
+                                    new Row("kellrott", "10-May-2012"),
+                                    new Row("elinor", "10-May-2012"),
+                                    new Row("ted", "01-May-2012")};
 
-	  final Panel panel = new HorizontalPanel();
-	  Panel userStatPanel = new VerticalPanel();
-	  
-	  Widget activeUCSCUsers = makeTableWithHeader("soe.ucsc.org - 4 active users", testUsersUCSC, testUserDateUCSC);
-	  Widget activeSageUsers = makeTableWithHeader("sagebase.org - 7 active users", testUsersSage, testUserDateSage);
-	  
-	  Widget activeProjects = makeTableWithHeader("Projects", projects, projectScore); 
-	  
-	  userStatPanel.add(activeUCSCUsers);
-	  userStatPanel.add(activeSageUsers);
-	  
-	  panel.add(userStatPanel);
-	  panel.add(activeProjects);
-	  RootPanel.get("statTableContainer").add(panel);
-  }
-  
-  public Widget makeTableWithHeader(String header, String[] col1, String[] col2) {
-	  FlowPanel panel = new FlowPanel();
-	  FlexTable table = new FlexTable();
-	  InlineLabel label = new InlineLabel();
-	  
-	  label.setText(header);
-	  panel.add(label);
-	  
-	  panel.addStyleName("margin-table");
-	  table.getRowFormatter().setStylePrimaryName(0, "header-row");
-	  for (int i = 0; i < col1.length; i++) {
-		  table.setText(i, 0, col1[i]);
-		  table.setText(i, 1, col2[i]);
-	  }
-	  
-	  panel.add(table);
-	  return panel;
-  }
+        Row[] sageData = new Row[] {new Row("chris.gaiteri", "18-May-2012"),
+                                    new Row("xudong.dai", "16-Apr-2012"),
+                                    new Row("charles.ferte", "27-Mar-2012"),
+                                    new Row("ben.sauerwine", "23-Apr-2012"),
+                                    new Row("in.sock.jang", "15-Mar-2012"),
+                                    new Row("erich.huang", "14-Mar-2012"),
+                                    new Row("lara.mangravite", "15-Mar-2012")};
+
+        RowTable ucscTable = new RowTable(2, "soe.ucsc.org -- 4 active users");
+        RowTable sageTable = new RowTable(2, "sagebase.org -- 7 active users");
+
+        Row header = new Row("User", "Last Sign-In");
+        ucscTable.setHeadings(header);
+        sageTable.setHeadings(header);
+
+        ucscTable.setRows(ucscData);
+        sageTable.setRows(sageData);
+
+        Row[] projectData = new Row[] {new Row("Name","Score"),
+                                       new Row("Metagenomics", "11"),
+                                       new Row("Synapse Umbrella", "4")};
+        RowTable projectTable = new RowTable(2, "Projects");
+
+        projectTable.setHeadings(new Row("Name", "Score"));
+        projectTable.setRows(projectData);
+
+        final Panel panel = new HorizontalPanel();
+        Panel userStatPanel = new VerticalPanel();
+
+        userStatPanel.add(ucscTable);
+        userStatPanel.add(sageTable);
+
+        panel.add(userStatPanel);
+        panel.add(projectTable);
+
+        RootPanel.get("statTableContainer").add(panel);
+    }
+
 }
--- src/main/java/org/sagebionetworks/metrics/client/widget/RowTable.java
+++ src/main/java/org/sagebionetworks/metrics/client/widget/RowTable.java
@@ -8,63 +8,75 @@ import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class RowTable extends Composite {
-	public class Row {
-		private final Object[] rowData;
-		
-		Row(Object... rowData) {
-			this.rowData = rowData;
-		}
+    public static class Row {
+        private final Object[] rowData;
+        
+        public Row(Object... rowData) {
+            this.rowData = rowData;
+        }
 
-		String getData(int column) {
-			return rowData[column].toString(); 
-		}
-	}
+        public String getData(int column) {
+            return rowData[column].toString(); 
+        }
+    }
 
-	private final int numColumns;
-	private InlineLabel heading = new InlineLabel();
-	private FlexTable table = new FlexTable();
-	private Row tableHeadings;
-	
-	RowTable(int numColumns) {
-		this.numColumns = numColumns;
-		setup();
-	}
-	
-	RowTable (String heading, int numColumns) {
-		this.numColumns = numColumns;
-		this.heading.setText(heading);
-		setup();
-	}
-	
-	private void setup() {
-		VerticalPanel panel = new VerticalPanel();
+    private final int numColumns;
+    private InlineLabel heading = new InlineLabel();
+    private FlexTable table = new FlexTable();
+    private Row tableHeadings;
+    
+    public RowTable(int numColumns) {
+        this.numColumns = numColumns;
+        setup();
+    }
+    
+    public RowTable (int numColumns, String heading) {
+        this.numColumns = numColumns;
+        this.heading.setText(heading);
+        setup();
+    }
+    
+    private void setup() {
+        VerticalPanel panel = new VerticalPanel();
 
-		// Initialize the header row to be a list of empty strings
-		// May not actually be necessary, but unlikely to have 
-		// performance implications
-		ArrayList<String> emptyArray = new ArrayList<String>();
-		for (int i = 0; i < numColumns; i++) {
-			emptyArray.add("");
-		}
+        // Initialize the header row to be a list of empty strings
+        // May not actually be necessary, but unlikely to have 
+        // performance implications
+        ArrayList<String> emptyArray = new ArrayList<String>();
+        for (int i = 0; i < numColumns; i++) {
+            emptyArray.add("");
+        }
 
-		setHeadings(this.new Row(emptyArray.toArray()));
-		table.getRowFormatter().setStylePrimaryName(0, "header-row");
+        setHeadings(new Row(emptyArray.toArray()));
+        table.getRowFormatter().setStylePrimaryName(0, "header-row");
 
-		panel.addStyleName("margin-table");
-		panel.add(heading);
-		panel.add(table);
+        panel.addStyleName("margin-table");
+        panel.add(heading);
+        panel.add(table);
 
-		initWidget(panel);
-	}
-	
-	public void setHeadings(Row headings) {
-		tableHeadings = headings;
-		setRow(0, tableHeadings);
-	}
-	
-	public void setRow(int row, Row data) {
-		for (int i = 0; i < numColumns; i++) {
-			table.setText(row, i, data.getData(i));
-		}
-	}
+        initWidget(panel);
+    }
+    
+    public void setHeadings(Row headings) {
+        tableHeadings = headings;
+        setRow(0, tableHeadings);
+    }
+    
+    public void setRow(int row, Row data) {
+        for (int i = 0; i < numColumns; i++) {
+            table.setText(row, i, data.getData(i));
+        }
+    }
+
+    public void setRows(Row[] rows) {
+        setRows(1, rows);
+    }
+
+    public void setRows(int startRow, Row[] rows) {
+        int i = startRow;
+        for (Row row : rows) {
+            setRow(i, row);
+            i++;
+        }
+    }
 }
